95a217ba67ac8fc7fc4ffdc52b5a5eb938e8c170
John Lees-Miller <john@jlm-squeeze.localdomain>
2011-05-04 23:21:35 +0100

    more reorganising...

---
+++ src/de/trafficsimulation/core/IDM.java
@@ -0,0 +1,106 @@
+package de.trafficsimulation.core;
+
+
+
+/**
+Basis class for the microscopic traffic model IDM 
+(intelligent-driver model, 
+see <a href="http://xxx.uni-augsburg.de/abs/cond-mat/0002177">
+M. Treiber, A. Hennecke, and D. Helbing, Congested Traffic States in Empirical Observations and Microscopic Simulations, 
+Phys. Rev. E 62, 1805 (2000)].</a>
+<br><br>
+    The classes IDMCar, IDMTruck, etc are concrete realisations 
+of this class for trucks, cars, etc. 
+
+*/
+
+public abstract class IDM implements MicroModel{
+
+
+
+    public double v0;
+    public double delta;
+    public double a;
+    public double b;
+    public double s0;
+    public double s1;
+    public double T;
+    public double sqrtab;
+    private static final int ismax=100;          // ve(s)=ve(ismax) for s>ismax assumed
+    private double[] veqTab = new double[ismax+1]; // table in steps of ds=1m
+
+    public IDM(){; }
+
+    public void set_v0(double v0){this.v0=v0;}
+    public void set_T(double T){this.T=T;}
+    public void set_a(double a){this.a=a;}
+    public void set_b(double b){this.b=b;}
+    public void set_s0(double s0){this.s0=s0;}
+    public void set_s1(double s1){this.s1=s1;}
+
+    public void set_params(IDM idm){
+	this.set_v0(idm.v0);
+	this.set_a(idm.a);
+	this.set_b(idm.b);
+	this.set_T(idm.T);
+	this.set_s0(idm.s0);
+	this.set_s1(idm.s1);
+    }
+
+    // calculate table of equilibrium velocity (s) with relaxation method;
+    // veqTab[i] = equilibrium velocity for s=i*ds with ds=1 m, i=0..100
+
+    public void initialize(){
+        final double dt=0.5;             // relaxation with timestep=0.5 s
+        final double kmax=20;            // number of iterations in rlaxation
+	veqTab[0]=0.0;
+	for (int is=1; is<=ismax ; is++){     // table in steps of ds=1 (m)
+
+	    double Ve=veqTab[is-1];
+            // if(is>=ismax-3){System.out.println("is="+is+" Ve="+Ve);}
+
+	    for (int k=0; k<kmax; k++){
+		double s_star = s0 + s1*Math.sqrt(Ve/v0)+Ve*T;	    
+		double acc=
+		    a * (1 - Math.pow((Ve/v0),delta) - (s_star*s_star)/(is*is) );
+		Ve=Ve+acc*dt;
+		if (Ve<0.0){Ve=0.0;}
+	    }
+	    veqTab[is]=Ve;
+	}
+        System.out.println("IDM.initialize():"
+        + "  veqTab[0]="+veqTab[0]
+        + ", veqTab["+ismax+"]="+veqTab[ismax]
+        );
+	
+    }
+
+    // function for equilibrium velocity using above table; ve(s>ismax)=ve(ismax)
+ 
+    public double Veq(double dx){
+	int is = (int) dx;
+	double V=0.0;
+	if (is<ismax){
+	    double rest=dx-((double) is);
+	    V = (1-rest)*veqTab[is] + rest*veqTab[is+1];
+	}
+	if (is>=ismax){ V=veqTab[ismax];}
+	if (is<=0){V=0.0;}
+	return V;
+    }
+
+    public double calcAcc(Moveable bwd, Moveable vwd){
+        double delta_v=bwd.velocity()-vwd.velocity();
+        double s=vwd.position()-bwd.position()-bwd.length();  // pos: END of vehicles!
+        double vel = bwd.velocity();
+        double s_star_raw = s0 + s1*Math.sqrt(vel/v0)+vel*T
+          + (vel*delta_v)/(2*sqrtab);
+        double s_star = (s_star_raw > s0) ? s_star_raw : s0;
+        double acc = a * (1 - Math.pow((vel/v0),delta) -(s_star*s_star)/(s*s) );
+        if (acc < -Constants.MAX_BRAKING) {
+            acc = -Constants.MAX_BRAKING;
+        }
+        return acc;	
+    }
+
+ }
---
+++ src/de/trafficsimulation/core/LaneChange.java
@@ -0,0 +1,133 @@
+package de.trafficsimulation.core;
+
+// MicroApplet2_0
+
+
+
+/**
+Implementation of the lane-changing model
+MOBIL ("Minimizing Overall Brakings Induced by Lane-changes"), see
+<a href="http://141.30.51.183/~treiber/publications/MOBIL.pdf">
+M. Treiber and D. Helbing, Realistische Mikrosimulation von Stra�enverkehr mit einem einfachen Modell </a>,
+16. Symposium "Simulationstechnik ASIM 2002" Rostock, 10.09 -13.09.2002, edited by Djamshid Tavangarian and Rolf Gr\"utzner pp. 514--520.
+
+*/
+
+public class LaneChange implements Constants{
+
+    // !! p and db and bsave overwritten by set_p, set_db!!
+
+    private double p=P_FACTOR_CAR; // politeness factor
+    private double db=DB_CAR;      // changing threshold
+    private double gapMin=MAIN_SMIN;       // minimum safe (net) distance
+    private double bsave=MAIN_BSAVE;     // maximum safe braking deceleration for other vehicles
+    private double bsaveSelf=MAIN_BSAVE_SELF;     // maximum safe braking deceleration for subject vehicle
+
+    // asymmetric European parts
+
+    //private double biasRight = BIAS_RIGHT_CAR; //bias (m/s^2) to drive right ln
+    private double biasRight = 0; // overwritten
+
+    final int LEFT=0;             // left lane=0
+    final int RIGHT=1;             
+
+
+    public LaneChange (String lctype){
+        if( lctype.equals("truck")){
+           System.out.println("in Cstr LaneChange(''truck''): "
+                            +"db=" +db+" bsave="+bsave);
+           p=P_FACTOR_TRUCK;  //!! overwritten in sliders
+           db=DB_TRUCK;  //!! overwritten in sliders
+           gapMin=MAIN_SMIN;
+           bsave=MAIN_BSAVE;
+           biasRight=BIAS_RIGHT_TRUCK;
+	}
+
+        else{         // default
+          System.out.println("in Cstr of LaneChange(''car''): "
+                            +"db=" +db+" bsave="+bsave);
+	}
+    }
+
+
+    public LaneChange (double p, double db, 
+                       double gapMin, double bsave, double biasRight){
+        this(p,db);
+        assert bsave <= MAX_BRAKING;
+        this.gapMin=gapMin;
+        this.bsave=bsave;
+        this.biasRight=biasRight;
+        System.out.println("Cstr of LaneChange(5 args): db="+db+" bsave="+bsave);
+    }
+
+    public LaneChange (double p, double db){
+        assert bsaveSelf <= MAX_BRAKING;
+        this.p=p;
+        this.db=db;
+        System.out.println("Cstr of LaneChange(5 args): db="+db+" bsave="+bsave);
+    }
+
+    public void set_p(double p){this.p=p;
+    //System.out.println("LaneChange.set_p: p="+p);
+    }
+    public void set_db(double db){this.db=db;
+    //System.out.println("LaneChange.set_db: db="+db);
+    }
+    public double get_p(){return(p);}
+    public double get_db(){return(db);}
+    public void set_gapMin(double gapMin){this.gapMin=gapMin;
+    //System.out.println("LaneChange.set_gapMin: gapMin="+gapMin);
+    }
+
+    public void set_bsave(double bsave){
+        assert bsave <= MAX_BRAKING;
+        this.bsave=bsave;
+      //System.out.println("LaneChange.set_bsave: bsave="+bsave);
+    }
+
+    public void set_biasRight(double bias){this.biasRight=bias;
+    //System.out.println("LaneChange.set_biasRight: biasRight="+biasRight);
+    }
+
+
+    public boolean changeOK(Moveable me, Moveable fOld, 
+        Moveable fNew, Moveable bNew) {
+
+        // fOld: front veh on old lane; 
+        // fNew,bNew: front,back vehicle on new lane; 
+
+        double gapFront=fNew.position() - me.position() - me.length();
+        if (gapFront <= gapMin) {
+            return false;
+        }
+        double gapBack=me.position() - bNew.position() - bNew.length();
+        if (gapBack <= gapMin) {
+            return false;
+        }
+
+        // safety criterion (a>-bsave); 
+        double bNew_acc = bNew.model().calcAcc(bNew,me);
+        if (bNew_acc < -bsave) {
+            return false;
+        }
+        double my_acc = me.model().calcAcc(me,fNew);
+        if (my_acc < -bsaveSelf) {
+            return false;
+        }
+
+        // incentive criterion (always model of BACK vehicle used!!)
+        // works also for on-ramp: on-ramp has 1 lane with index 0
+        // === LEFT on main road -> strong desired to change = 
+        // large positive biasRight for lcModel of ramp vehicles
+
+        double my_adv  = my_acc - me.model().calcAcc(me,fOld)
+                       + ((me.lane()==LEFT) ? 1 : -1) * biasRight;
+
+        double others_disadv = bNew.model().calcAcc(bNew,fNew) - bNew_acc;
+
+        if (others_disadv<0) {
+            others_disadv=0;
+        }
+        return my_adv - p*others_disadv > db ? true : false;
+    }
+}
