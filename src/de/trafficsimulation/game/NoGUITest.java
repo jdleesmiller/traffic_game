b2ef72814c13b970779585082f77396e457dcd68
John Lees-Miller <enjdlm@enm-jdlm.(none)>
2011-06-25 17:57:17 +0100

    added qRamp and speed limit params

--- src/de/trafficsimulation/game/NoGUITest.java
+++ src/de/trafficsimulation/game/NoGUITest.java
@@ -12,39 +12,63 @@ import de.trafficsimulation.core.Constants;
  */
 public class NoGUITest implements Constants {
   
+  private static final int MAX_Q_IN = 3000; // vehicles / hour
+  
   public static int runForNumCarsOut(
-      URoadSim sim, double statsStartTime, double statsDuration)
+      URoadSim sim, double statsStart, double statsDuration)
   {
-    int numCarsOutInWarmup = -1;
-    double statsEndTime = statsStartTime + statsDuration;
-    while (sim.getTime() < statsEndTime) {
-      if (numCarsOutInWarmup < 0 && sim.getTime() > statsStartTime)
-        numCarsOutInWarmup = sim.getStreet().getNumCarsOut();
-      sim.tick();
+    try {
+      while (sim.getTime() < statsStart) {
+        sim.tick();
+      }
+      sim.getStreet().resetNumCarsOut();
+      while (sim.getTime() < statsStart + statsDuration) {
+        sim.tick();
+      }
+      return sim.getStreet().getNumCarsOut();
+    } catch(Throwable e) {
+      e.printStackTrace(System.err);
+      return -1;
     }
-    return sim.getStreet().getNumCarsOut() - numCarsOutInWarmup;
   }
   
-  public static void sweepQIn(double statsStartTime, double statsDuration,
-      int numTrials, int numPoints) {
+  public static void sweepQIn(long seed, double statsStart,
+      double statsDuration, int numTrials, int numPoints, double rampFlow,
+      double speedLimit) {
+    // lengths taken from URoadCanvas
     double uRoadLengthMeters = 1176.9911184307753;
     double rampLengthMeters = 270.0;
     
-    System.out.println("statsDuration\tqIn\tnumCarsOut\trunMillis");
+    String header = "seed statsStart statsDuration qIn rampFlow speedLimit"
+        + " numCarsOut runMillis";
+    System.out.println(header.replace(' ', '\t'));
     
-    Random random = new Random();
+    Random random;
+    if (seed == -1L)
+      random = new Random();
+    else
+      random = new Random(seed);
+      
     for (int trial = 0; trial < numTrials; ++trial) {
-      for (int point = 0; point < numPoints; ++point) {
+      for (int point = 1; point <= numPoints; ++point) {
         URoadSim sim = new URoadSim(random,
             uRoadLengthMeters, rampLengthMeters);
-        sim.qIn = ((double)point) * Q_MAX / 3600.0 / numPoints;
+        sim.qIn = ((double)point) * MAX_Q_IN / 3600.0 / numPoints;
+        sim.qRamp = rampFlow / 3600.0;
+        sim.setSpeedLimit(Utility.milesPerHourToMetersPerSecond(speedLimit));
         long startTime = System.currentTimeMillis();
-        int numCarsOut = runForNumCarsOut(sim, statsStartTime, statsDuration);
+        int numCarsOut = runForNumCarsOut(sim, statsStart, statsDuration);
         long endTime = System.currentTimeMillis();
-        System.out.println(statsDuration +
-        		"\t" + (sim.qIn * 3600.0) +
-        		"\t" + numCarsOut + 
-        		"\t" + (endTime - startTime));
+        
+        System.out.print(seed);
+        System.out.print("\t" + statsStart);
+        System.out.print("\t" + statsDuration);
+        System.out.print("\t" + sim.qIn * 3600.0);
+        System.out.print("\t" + rampFlow);
+        System.out.print("\t" + speedLimit);
+        System.out.print("\t" + numCarsOut);
+        System.out.print("\t" + (endTime - startTime));
+        System.out.println();
       }
     }
   }
@@ -53,24 +77,30 @@ public class NoGUITest implements Constants {
    * @param args
    */
   public static void main(String[] args) {
-    double statsStartMins = 30;
-    double statsDurationMins = 60;
+    long seed = -1L;
+    double statsStart = 30*60;
+    double statsDuration = 60*60;
     int numTrials = 10;
     int numPoints = 100;
+    double rampFlow = 400; // vehicles/hour
+    double speedLimit = 70; // mph
     
     if (args.length > 0)
-      statsStartMins = Double.parseDouble(args[0]);
+      seed = Long.parseLong(args[0]);
     if (args.length > 1)
-      statsDurationMins = Double.parseDouble(args[1]);
+      statsStart = Double.parseDouble(args[1]);
     if (args.length > 2)
-      numTrials = Integer.parseInt(args[2]);
+      statsDuration = Double.parseDouble(args[2]);
     if (args.length > 3)
-      numPoints = Integer.parseInt(args[3]);
-    
-    long startTime = System.currentTimeMillis();
-    sweepQIn(statsStartMins * 60, statsDurationMins * 60, numTrials, numPoints);
-    long stopTime = System.currentTimeMillis();
-    long runTime = stopTime - startTime;
-    System.out.println("Run time: " + runTime + "ms");
+      numTrials = Integer.parseInt(args[3]);
+    if (args.length > 4)
+      numPoints = Integer.parseInt(args[4]);
+    if (args.length > 5)
+      rampFlow = Double.parseDouble(args[5]);
+    if (args.length > 6)
+      speedLimit = Double.parseDouble(args[6]);
+      
+    sweepQIn(seed, statsStart, statsDuration, numTrials,
+        numPoints, rampFlow, speedLimit);
   }
 }
